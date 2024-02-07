c646313c9f23661334156eff919b1aef2ce911be
Kunal <kunalpawar0296@gmail.com>
2024-02-07 15:06:31 +0530

    Dashboard changes and announcement component added

--- sms/src/components/Announcements.js
+++ sms/src/components/Announcements.js
@@ -1,9 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import {
+  Button,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import Divider from "@mui/material/Divider";
 
 function Announcements() {
-    return ( <div style={{position:"absolute",top:"50%",left:"50%",color:"#222831"}}>
-        <h1>Announcements</h1>
-    </div> );
+  const [announ, setAnnoun] = useState("");
+
+  const handleAnnounClick = (e) => {
+    setAnnoun(`Hello ${e.target.value}`);
+  };
+
+  return (
+    <Paper
+      sx={{
+        position: "absolute",
+        marginTop: "150px",
+        marginLeft: "200px",
+        color: "#222831",
+        width: "inherit",
+      }}
+      elevation={6}
+    >
+      <Stack direction="row">
+        <Box
+          sx={{
+            width: "20vw",
+            height: "700px",
+            bgcolor: "#222831",
+            color: "#EEEEEE",
+            overflow: "auto",
+          }}
+        >
+          <List>
+            {[1, 2, 3, 4, 5].map((value) => {
+              const labelValue = `Announcement ${value}`;
+
+              return (
+                <ListItem
+                  disablePadding
+                  key={value}
+                  onClick={handleAnnounClick}
+                >
+                  <ListItemButton>
+                    <ListItemText primary={labelValue} />
+                  </ListItemButton>
+                </ListItem>
+              );
+            })}
+          </List>
+        </Box>
+
+        <Box sx={{ width: "60vw", bgcolor: "white" }}>
+          <Typography variant="h5">{announ}</Typography>
+        </Box>
+      </Stack>
+    </Paper>
+  );
 }
 
 export default Announcements;
