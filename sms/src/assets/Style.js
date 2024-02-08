422c41edc8751fb2ada251c9d182d6ece7c108f4
Kunal <kunalpawar0296@gmail.com>
2024-02-08 23:18:20 +0530

    Style js file added to store global values

---
+++ sms/src/assets/Style.js
@@ -0,0 +1,11 @@
+const styles = {
+  colors: {
+    primary: "#EEEEEE",
+    primary_medium: "#D9D9D9",
+    primary_dark: "#222831",
+    secondary: "#00ADB5",
+    secondary_dark: "#393E46",
+  },
+};
+
+export default styles;
