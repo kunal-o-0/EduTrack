e335689459e8b50fbbb12e9437381883e2b373fe
Kunal <kunalpawar0296@gmail.com>
2024-02-09 13:14:01 +0530

    Attendance and Grades components added

--- sms/src/components/Attendance.js
+++ sms/src/components/Attendance.js
@@ -1,7 +1,135 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import Styles from "../assets/Style";
+
+const columns = [
+  {
+    id: "date",
+    label: "Date",
+    align: "center",
+    minWidth: "9rem",
+  },
+  {
+    id: "day",
+    label: "Day",
+    align: "center",
+    minWidth: "9rem",
+  },
+  {
+    id: "status",
+    label: "Status",
+    align: "center",
+    minWidth: "9rem",
+  },
+];
+
+function createData(date, day, status) {
+  return { date, day, status };
+}
+
 function Attendance() {
-    return ( <div style={{position:"absolute",top:"50%",left:"50%",color:"#222831"}}>
-        <h1>Attendance</h1>
-    </div> );
+  const data = [
+    createData("24-05-2023", "Monday", "Present"),
+    createData("27-12-2023", "Friday", "Present"),
+    createData("15-08-2023", "Tuesday", "Absent"),
+    createData("2-01-2023", "Wednesday", "Present"),
+    createData("14-02-2023", "Tuesday", "Absent"),
+    createData("04-05-2023", "Thursday", "Absent"),
+    createData("24-05-2023", "Monday", "Present"),
+    createData("27-12-2023", "Friday", "Present"),
+    createData("15-08-2023", "Tuesday", "Absent"),
+    createData("2-01-2023", "Wednesday", "Present"),
+    createData("14-02-2023", "Tuesday", "Absent"),
+    createData("04-05-2023", "Thursday", "Absent"),
+    createData("24-05-2023", "Monday", "Present"),
+    createData("27-12-2023", "Friday", "Present"),
+    createData("15-08-2023", "Tuesday", "Absent"),
+    createData("2-01-2023", "Wednesday", "Present"),
+    createData("14-02-2023", "Tuesday", "Absent"),
+    createData("04-05-2023", "Thursday", "Absent"),
+  ];
+
+  return (
+    <Paper
+      sx={{
+        position: "absolute",
+        width: "85%",
+        left: "10rem",
+        top: "9rem",
+        overflowY: "auto",
+      }}
+      elevation={0}
+    >
+      <TableContainer sx={{ maxHeight: "45rem" }}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => {
+                return (
+                  <TableCell
+                    align={column.align}
+                    key={column.id}
+                    sx={{
+                      bgcolor: `${Styles.colors.primary_dark}`,
+                      color: `${Styles.colors.primary}`,
+                      minWidth: column.minWidth,
+                    }}
+                  >
+                    <Typography
+                      variant="h5"
+                      component="p"
+                      sx={{ fontSize: "1.4rem", fontWeight: "900" }}
+                    >
+                      {column.label}
+                    </Typography>
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row) => {
+              return (
+                <TableRow>
+                  {columns.map((column) => {
+                    return (
+                      <TableCell
+                        align={column.align}
+                        sx={{
+                          bgcolor: `${
+                            row.status === "Present"
+                              ? Styles.colors.success
+                              : Styles.colors.error
+                          }`,
+                          color: Styles.colors.primary,
+                        }}
+                      >
+                        <Typography
+                          variant="subtitle1"
+                          component="p"
+                          sx={{ fontSize: "1.1rem", fontWeight: "600" }}
+                        >
+                          {row[column.id]}
+                        </Typography>
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
 }
 
 export default Attendance;
--- sms/src/components/Grades.js
+++ sms/src/components/Grades.js
@@ -1,7 +1,142 @@
+import {
+  Box,
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import Styles from "../assets/Style";
+
+const Columns = [
+  { id: "subject", label: "Subject", minWidth: "20rem", align: "center" },
+  {
+    id: "obtained_marks",
+    label: "Obtained Marks",
+    minWidth: "15rem",
+    align: "center",
+  },
+  {
+    id: "total_marks",
+    label: "Total Marks",
+    minWidth: "15rem",
+    align: "center",
+  },
+  { id: "grades", label: "Grades", minWidth: "5rem", align: "center" },
+];
+
+function createData(subject, obtained_marks, total_marks, grades) {
+  return { subject, obtained_marks, total_marks, grades };
+}
+
 function Grades() {
-    return ( <div style={{position:"absolute",top:"50%",left:"50%",color:"#222831"}}>
-        <h1>Grades</h1>
-    </div> );
+  const data = [
+    createData("Maths", 553, 600, "A+"),
+    createData("Science", 553, 600, "A+"),
+    createData("History", 553, 600, "A+"),
+    createData("Geography", 553, 600, "A+"),
+    createData("Engineering Mathematics", 553, 600, "A+"),
+    createData("English", 553, 600, "A+"),
+    createData("Maths", 553, 600, "A+"),
+    createData("Maths", 553, 600, "A+"),
+    createData("Java", 553, 600, "A+"),
+    createData("Python", 553, 600, "A+"),
+    createData("Maths", 553, 600, "A+"),
+    createData("HTML", 553, 600, "A+"),
+  ];
+
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        left: "9rem",
+        top: "5rem",
+        width: "85%",
+      }}
+    >
+      <Typography
+        variant="h3"
+        align="center"
+        sx={{ fontSize: "2rem", fontWeight: "900" }}
+        gutterBottom
+      >
+        Course Name
+      </Typography>
+      <Paper sx={{ width: "100%" }}>
+        <TableContainer sx={{ overflowY: "auto", maxHeight: "45rem" }}>
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                {Columns.map((column) => {
+                  return (
+                    <TableCell
+                      align={column.align}
+                      key={column.id}
+                      sx={{
+                        minWidth: column.minWidth,
+                        bgcolor: Styles.colors.primary_dark,
+                      }}
+                    >
+                      <Typography
+                        variant="h5"
+                        component="p"
+                        sx={{
+                          fontSize: "1.5rem",
+                          fontWeight: "600",
+                          color: Styles.colors.primary,
+                        }}
+                      >
+                        {column.label}
+                      </Typography>
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((row) => {
+                return (
+                  <TableRow>
+                    {Columns.map((column) => {
+                      return (
+                        <TableCell
+                          align={
+                            column.id === "subject" ? "left" : column.align
+                          }
+                        >
+                          <Typography
+                            variant="subtitle1"
+                            component="p"
+                            sx={{ fontSize: "1.1rem", fontWeight: "300" }}
+                          >
+                            {row[column.id]}
+                          </Typography>
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+      <Box display="flex" justifyContent="center">
+        <Button
+          variant="contained"
+          sx={{ marginTop: "1.5rem", bgcolor: Styles.colors.secondary }}
+        >
+          <Typography variant="h6" sx={{ fontSize: "1rem", fontWeight: "900" }}>
+            Generate Report
+          </Typography>
+        </Button>
+      </Box>
+    </Box>
+  );
 }
 
 export default Grades;
