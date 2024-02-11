9949f22c675509041ffca31947921c1dfc6f1e4f
Kunal <kunalpawar0296@gmail.com>
2024-02-11 23:40:21 +0530

    data annotation added for getter and setter

--- Backend/sms_backend/src/main/java/com/app/entities/secondary/Address.java
+++ Backend/sms_backend/src/main/java/com/app/entities/secondary/Address.java
@@ -3,7 +3,10 @@ package com.app.entities.secondary;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
+import lombok.Data;
+
 @Embeddable
+@Data
 public class Address {
 	@Column(name = "address_line",length=150)
 	private String addressLine;
