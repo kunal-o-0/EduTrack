5bba5ed423f70b80c14920a22fa4217519e6d1b0
Kunal <kunalpawar0296@gmail.com>
2024-02-16 22:16:02 +0530

    data type of Schedule changed from util.Date to sql.Date

--- Backend/sms_backend/src/main/java/com/app/dto/schedule/SchedDto.java
+++ Backend/sms_backend/src/main/java/com/app/dto/schedule/SchedDto.java
@@ -1,7 +1,7 @@
 package com.app.dto.schedule;
 
 import java.sql.Time;
-import java.util.Date;
+import java.sql.Date;
 
 import javax.validation.constraints.NotNull;
 
--- Backend/sms_backend/src/main/java/com/app/entities/secondary/Schedule.java
+++ Backend/sms_backend/src/main/java/com/app/entities/secondary/Schedule.java
@@ -1,7 +1,7 @@
 package com.app.entities.secondary;
 
 import java.sql.Time;
-import java.util.Date;
+import java.sql.Date;
 import java.util.List;
 
 import javax.persistence.Column;
