ec17080710e5c7ab2f612ffd359f4945b61baf05
Kunal <kunalpawar0296@gmail.com>
2024-02-17 16:55:25 +0530

    cross orgin added to student controller

--- Backend/sms_backend/src/main/java/com/app/controller/StudentController.java
+++ Backend/sms_backend/src/main/java/com/app/controller/StudentController.java
@@ -9,6 +9,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ import com.app.util.ResponseText;
 
 @RestController
 @RequestMapping("/student")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class StudentController {
 	@Autowired
 	private ModelMapper mapper;
